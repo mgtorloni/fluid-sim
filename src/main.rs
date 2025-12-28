a596266efcda0746fc1541614d611913c1d50668
mgtorloni <matheus.gil.torloni@hotmail.com>
2025-12-28 18:00:55 +0000

    made it cuter. Will start doing proper SPH now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use macroquad::prelude::*;
 
 const RADIUS: f32 = 4.0;
-const PPM: f32 = 20.0; // pixels per metre, play with this value. This makes sense right now
+const PPM: f32 = 20.0; // pixels per metre, play with this value. This value makes sense right now
 const GRAVITY: f32 = 9.80 * PPM;
 const RESTITUTION: f32 = 0.5;
 
@@ -22,39 +22,37 @@ impl Particles {
         self.vel.push(vec2(vx, vy));
     }
 
-    fn collide(&mut self) {
-        let count = self.pos.len();
-        for i in 0..count {
-            for j in i + 1..count {
-                let delta_length = self.pos[i] - self.pos[j];
-                let dist_sq = delta_length.length_squared();
-                let min_dist = RADIUS * 2.0;
+    fn collide(&mut self, i: usize, j: usize) -> bool {
+        let delta_length = self.pos[i] - self.pos[j];
+        let dist_sq = delta_length.length_squared();
+        let min_dist = RADIUS * 2.0;
 
-                if dist_sq < min_dist * min_dist {
-                    // distance between two particles diameter, we collide particles
-                    let dist = dist_sq.sqrt();
-                    let normal = delta_length / dist;
-                    let rel_vel = self.vel[i] - self.vel[j];
-                    let vel_along_normal = rel_vel.dot(normal);
-                    if vel_along_normal > 0.0 {
-                        continue;
-                    }
-
-                    let impulse_mag = -(1.0 + RESTITUTION) * vel_along_normal / 2.0;
-
-                    let impulse = normal * impulse_mag;
-                    self.vel[i] += impulse;
-                    self.vel[j] -= impulse;
-
-                    let percent = 0.8;
-                    let penetration = min_dist - dist;
-                    let correction = normal * (penetration / 2.0) * percent;
-
-                    self.pos[i] += correction;
-                    self.pos[j] -= correction;
-                }
+        if dist_sq < min_dist * min_dist {
+            // distance between two particles diameter, we collide particles
+            let dist = dist_sq.sqrt();
+            let normal = delta_length / dist;
+            let rel_vel = self.vel[i] - self.vel[j];
+            let vel_along_normal = rel_vel.dot(normal);
+            if vel_along_normal > 0.0 {
+                // Particle must be going in opposite direction to the one it will hit
+                //otherwise  no collision is happening
+                return false;
             }
+
+            let impulse_mag = -(1.0 + RESTITUTION) * vel_along_normal / 2.0;
+
+            let impulse = normal * impulse_mag;
+            self.vel[i] += impulse;
+            self.vel[j] -= impulse;
+
+            let percent = 0.8;
+            let penetration = min_dist - dist;
+            let correction = normal * (penetration / 2.0) * percent;
+
+            self.pos[i] += correction;
+            self.pos[j] -= correction;
         }
+        true
     }
 
     fn update(&mut self) {
@@ -76,19 +74,28 @@ impl Particles {
                 self.pos[i].y = screen_height() - RADIUS;
             }
         }
-        self.collide();
+        let count = self.pos.len();
+        for i in 0..count {
+            for j in i + 1..count {
+                let collision = self.collide(i, j);
+                if !collision {
+                    continue;
+                }
+            }
+        }
     }
     fn draw(&self) {
         let max_speed: f32 = 100.0;
 
         for i in 0..self.pos.len() {
             let ratio: f32 = (self.vel[i].length() / max_speed).min(1.0);
-            let colour = Color::new(
-                (ratio * 2.0).min(1.0),
-                1.0 - (ratio - 0.5).abs() * 2.0,
-                ((1.0 - ratio) * 2.0).min(1.0),
-                1.0,
-            );
+            // let colour = Color::new(
+            //     (ratio * 2.0).min(1.0),
+            //     1.0 - (ratio - 0.5).abs() * 2.0,
+            //     ((1.0 - ratio) * 2.0).min(1.0),
+            //     1.0,
+            // );
+            let colour = Color::new(ratio, 0.0, 1.0 - ratio, 1.0);
             draw_circle(self.pos[i].x, self.pos[i].y, RADIUS, colour);
         }
     }
@@ -117,7 +124,7 @@ async fn main() {
     }
 
     loop {
-        clear_background(DARKGRAY);
+        clear_background(WHITE);
         simulation.update();
         simulation.draw();
 
